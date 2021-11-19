a339a8e9a1253d9124fe9cd955790ca5c4ce0fe1
1dawu2 <david_wurm@web.de>
2021-11-19 12:43:59 +0100

    Change 3.2

--- box.js
+++ box.js
@@ -25,27 +25,40 @@
             this._props = {};
         }
 
+        _button = undefined;
+        setButtonText2(button, text) {
+            button.setText(text); // (2) OK
+            _button = button;
+            setTimeout(() => {
+                this._button.setText(text); // (3) WILL NOT WORK!
+            });
+        }
+
+        handleClick() {
+            this._button.setText("Hello!"); // (4) WILL NOT WORK!
+        }
+
+
         get tableName() {
             return this._tableName;
         }
         set tableName(tableName) {
-        	console.log("setTitle:" + tableName);
+            console.log("setTitle:" + tableName);
             this._tableName = tableName;
         }
 
 
-
-		async retrieveSource(source) {
-			this._dataSource = source;			
-            console.log(source);
-			let resultSet = await source.getResultSet();
+        async retrieveSource(newsource) {
+            this._dataSource = newsource;
+            console.log(newsource);
+            let resultSet = await newsource.getResultSet();
             console.log(resultSet);
             let value = result ? parseFloat(result["@MeasureDimension"].rawValue) : null;
-            console.log(value);	
+            console.log(value);
             console.log("Hallo World");
             alert("Hello! I am an alert box!!");
-			
-		}        
+
+        }
         onCustomWidgetBeforeUpdate(changedProperties) {
             this._props = { ...this._props, ...changedProperties };
         }
